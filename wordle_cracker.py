e539b3e66e0dddc553bdfd290c1c385e9c336b76
Haris Mallick <Haris.Mallick@touchlight.com>
2022-03-20 04:53:51 +0000

    Added vowel restriction condition

--- wordle_cracker.py
+++ wordle_cracker.py
@@ -3,13 +3,19 @@
 Created on Sun Mar 13 03:15:42 2022
 
 @author: Haris.Mallick
+
+Script to try and find the best 6-letter words to used in 6-letter wordle.
+Each word should be composed of unique letters and each word in 4-word subarray should have unique letters.
+This should allow one to utilise 24 of the 26 English letters in first 4 attempts of the game.
+The final two attempts can be used to guess the wordle of the day.
+
 """
 
 from english_words import english_words_lower_alpha_set
 
 print(len(english_words_lower_alpha_set)) #25463 words
 
-# file = open('English_words.txt') #only use this file once code is optimised. Has 10x more words!
+#file = open('English_words.txt') #only use this file once code is optimised. Has 10x more words!
 ##Append every 6-letter word in English to an array
 
 six_letter_list = []
@@ -31,9 +37,9 @@ print(len(six_letter_list)) #29874 6-letter words from .txt; 3909 from english_w
 #     'u', 'v','w','x','y','z'
 #     ]
 
-#ignore j and q -- least used letters in english language
+#z,q,j,x,k,v are the least used letters in English
 
-## Append words with all unique letters to a new array (except j, q)
+## Append words with all unique letters to a new array
 unique_letter_words = []
 i = 0
 
@@ -48,26 +54,31 @@ while i < len(six_letter_list):
         if x_ascii_sort[ii] == x_ascii_sort[ii+1]:
             dups = True
     
-    for letter in x_ascii_sort:
-        if letter == 'j' or letter == 'q':
-            j_q_check = True
+    # for letter in x_ascii_sort:
+    #     if letter == 'j' or letter == 'q':
+    #         j_q_check = True
         
-    if dups == False and j_q_check == False:
-        unique_letter_words.append(x)
+    if dups == False:# and j_q_check == False:
+        vowels = {'a', 'e', 'i', 'o', 'u', 'y'}
+        intersection = vowels.intersection(x)
+        if len(intersection) < 3: #ideally restrict search to words with max 1 vowel, but array size drastically reduced.
+            
+            unique_letter_words.append(x)
         # for letter in x:
         #     all_letters.remove(letter)
                 
     i += 1
         
-        
-print(len(unique_letter_words))    #1847 words (13308 from the text file)    
+#print(unique_letter_words)        
+print(len(unique_letter_words))    #408 (204) words with 1 vowel each #1847 words if multiple vowels allowed per word (13308 from the text file)    
 #print(unique_letter_words)
 
 ##Nested for loops to identify word combos using unique letters each and covering 24 of 26 English letters
 
+#vowels = {'a', 'e', 'i', 'o', 'u'} #find words that only contain one vowel each
 letter_coverage = []
 for word in unique_letter_words:
-    temp_list = []
+    temp_list = [] #when starting with 'crypts' got certain subarrays with a vowel unsed. Starting with 'zqjxkv' to test if letter usage statistics of English would help
     temp_list.append(word)
     
     for i in range(len(unique_letter_words)-1):
@@ -176,6 +187,30 @@ print(letter_coverage)
 
 # x = test_func()
 # print(x)
+
+# set_vowels = {'a', 'e', 'i', 'o', 'u'}
+# print(set_vowels)
+# # set_test.remove('a')
+# # print(set_test)
+# word = 'bat'
+# common = set_vowels.intersection(word)
+# print(common, len(common))
+# pop = common.pop()
+# set_vowels.remove(pop)
+# print(pop, set_vowels)
+
+# test_word = 'abrakadabra'
+# counter = {}
+# for letter in test_word:
+    
+#     if counter.get(letter) == None:
+#         counter[letter] = 1
+        
+#     else:
+#         counter[letter] +=1
+        
+# print(counter)
+
 
 
 
